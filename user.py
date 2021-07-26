e2f11f467e2ba6a071011b7f801a1092bb7559cf
oetam24 <matiwroblewski@gmail.com>
2021-07-26 17:14:40 +0100

    - wrzucenie nowej funkcjonalnosci

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, nickname, password):
+        self.nickname = nickname
+        self.password = password
+        self.messages = []
+
+    def send_message(self, nickname_to, text):
+        pass
+
+    def print_messages(self):
+        for message in selg.messages
+            print(f'*    {message.from_user} - {message.timestamp}')
+            print(message.text)
+            print()
